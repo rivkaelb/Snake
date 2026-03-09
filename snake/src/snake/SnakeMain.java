a03be20b047aad3274fe0d6a50dc16226e313d36
rivkaelb <rivkaelb@gmail.com>
2026-03-10 00:09:20 +0200

    Erase comments from SnakeMain.java

--- snake/src/snake/SnakeMain.java
+++ snake/src/snake/SnakeMain.java
@@ -3,21 +3,9 @@ package snake;
 public class SnakeMain {
 
 	public static void main(String[] args) {
-//		SnakeGameLogic snake = new SnakeGameLogic();
-//		snake.printBoard();
-//		snake.move(Direction.right);
-//		snake.printBoard();
-//		snake.move(Direction.up);
-//		snake.printBoard();
-//		snake.move(Direction.up);
-//		snake.printBoard();
-//		snake.move(Direction.down);
-//		snake.printBoard();
-//		snake.move(Direction.up);
-//		snake.printBoard();
-
 		Game game = new Game();
 		game.start();
 	}
 
 }
+
